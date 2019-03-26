570c4f9337f4ddafb8b9404a8001db566891b87f
godobby <godobby00@gmail.com>
2019-03-26 15:20:50 +0900

    UserRepository　interfaceまで

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -1,8 +1,19 @@
-package net.slipp.web;
+package net.slipp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
-	private String userId;
+	@Id //primary key 指定
+	@GeneratedValue // 自動で1ずつ増加するように（sequency）
+	private Long id;
+	
+	@Column(nullable=false, length=20)//null値が入れないように
+	private String userId; //primary key
 	private String password;
 	private String name;
 	private String email;
