ade95f2fac622227c0219190e8ed96818d14f861
godobby <godobby00@gmail.com>
2019-03-28 11:26:43 +0900

    個人情報修正の機能具現まで

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
@@ -18,7 +19,7 @@ import net.slipp.domain.UserRepository;
 @RequestMapping("/users")
 public class UserController {
 	
-	private List<User> users = new ArrayList<User>();
+	private List<User> users = new ArrayList<User>(); // DB를 사용하기 전 데이터를 일시적으로 담아둔 리스트
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -49,7 +50,7 @@ public class UserController {
 		return "/user/list";
 	}
 	
-	// 유저 개인정보 수정
+	// 유저 개인정보 수정화면
 	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model) {
 		// id로 DB에서 검색한 해당 사용자 정보를 Model에 담아서 updateForm.html파일에 보내주기
@@ -59,5 +60,26 @@ public class UserController {
 
 		return "/user/updateForm";
 	}
+	
+	// 유저 개인정보 수정
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser ) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
+	/*Html의 put메소드를 이용해 개인정보수정 (스프링에선 별도의 설정이필요, 부트에선 설정되있음) 
+	 * 
+	// 유저 개인정보 수정
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser ) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	*/
 
 }
