ccd9baee1c18331d6ee23d7b66c630baf4d4b8a8
godobby <godobby00@gmail.com>
2019-03-29 17:57:01 +0900

    QuestionList具現

--- src/main/java/net/slipp/web/HomeController.java
+++ src/main/java/net/slipp/web/HomeController.java
@@ -1,13 +1,22 @@
 package net.slipp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import net.slipp.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@GetMapping("")
-	public String home( ) {
+	public String home(Model model) {
+		// 모델에 questions라는 이름의 질문데이터를 담아서 view에 전달
+		model.addAttribute("questions", questionRepository.findAll());
 		return "index";
 	}
 
